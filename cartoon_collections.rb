ed4f2f4708f2c8ae6acda1d7f6f9d4bed4611bef
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-21 20:46:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,28 +1,29 @@
 def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
-def roll_call_dwarves(names)
-  names.each_with_index do |name,index|
-    puts "#{index+1}.#{name}"
-  end
+  array.each_with_index{|name, index|
+    puts "#{index + 1}. #{name}"
+  }
 end
  def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map{|i| i.capitalize + "!"}
+  array.collect{|element|
+    element.capitalize + "!"
+  }
 end
  def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
-def long_planeteer_calls(calls)
-  calls.any?{|word| word.length>4}
+  array.any?{|word|
+    word.length > 4
+  }
 end
  def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
-def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  for i in cheese_types
-  	if list.include?(i)
-  		return i
-  	end
-  end
-  return
+  array.find{|item|
+    cheese_types.include?(item)
+  }
 end
